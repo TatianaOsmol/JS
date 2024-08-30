c049893f74218eaa2ed5c5b63ef0ebcbe1b30626
Tatiana <asdamasu@gmail.com>
2024-08-30 13:45:59 +0300

    task4

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -16,9 +16,5 @@ const hasTemporaryPass = false
 let isAccess
 
 // your code
-const isAdmin = false;
-const isVerifiedUser = true;
-const hasSpecialPermission = true;
-const hasTemporaryPass = false;
 let isAccess = (isAdmin || isVerifiedUser) && (hasSpecialPermission || hasTemporaryPass)
 console.log(isAccess)
