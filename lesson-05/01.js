ee82459a9baceeef6585948d57d68698ff8b18b6
Tatiana <asdamasu@gmail.com>
2024-09-26 12:49:02 +0300

    task7

--- lesson-05/01.js
+++ lesson-05/01.js
@@ -15,5 +15,11 @@ const game = {
     gold: 250,
     lumber: 100,
   },
-  addResource() {}
+  addResource(resourse, amount) {
+    if (this.resources.hasOwnProperty(resourse)) {
+      this.resources[resourse] += amount;
+    } else {
+      console.log("Invalid resource");
+    }
+  }
 }
